c0eb18756bb9cf9a1da0764fcb484ff0ecfa69bc
Abigail <abigail@Melissas-MacBook-Pro.local>
2018-10-20 12:18:15 -0700

    add image uploading

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module LaAdventures
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoload_paths += %W(#{config.root}/app/uploaders)
   end
 end
